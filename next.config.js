9f7c2bff1ffdb9315c7f1a51cdb772d99af7a10d
pedrocatequeze-beep <pedrocatequeze@gmail.com>
2026-01-28 15:09:53 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ["ipfs.infura.io"],
+  },
+  webpack: (config) => {
+    // Resolver erro "digital envelope routines::unsupported"
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      crypto: false,
+    };
+    return config;
   },
 };
